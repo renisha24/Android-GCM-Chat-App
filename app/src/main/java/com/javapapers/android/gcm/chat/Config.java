f62bde849f6af23344fe7b2dfc58fd8358d0f6a6
Renisha <renisha24@gmail.com>
2017-10-16 16:14:43 +0530

    Initial commit

---
+++ app/src/main/java/com/javapapers/android/gcm/chat/Config.java
@@ -0,0 +1,9 @@
+package com.javapapers.android.gcm.chat;
+
+/**
+ * Created by Joe on 5/28/2014.
+ */
+public class Config {
+    // Google Project Number
+    static final String GOOGLE_PROJECT_ID = "519264447280";
+}
